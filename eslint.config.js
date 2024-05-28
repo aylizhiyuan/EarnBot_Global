b7cf01434ab3c4709e8ea6b1e05c8c0744e90f48
lizhiyuan <lizhiyuan2023@gmail.com>
2024-05-28 16:59:03 +0800

    modify eslint.config

---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+// eslint.config.js
+const { ESLint } = require('eslint')
+const js = require('@eslint/js')
+module.exports = []
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, () => {
+  // go to the server
   console.log(`server is listen on port 3000`)
 })
